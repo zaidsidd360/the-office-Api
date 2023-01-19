5293409af86e7d7c9d9fa6036fc8e524fc8ed76f
zaidsidd360 <zaidsidd360@gmail.com>
2023-01-20 03:35:48 +0530

    Initial commit

---
+++ officeapi/views.py
@@ -0,0 +1,13 @@
+from django.http import JsonResponse
+from .models import Character
+from .serializers import CharacterSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .characters import characters
+
+
+@api_view(['GET'])
+def get_charactes(request):
+    # characters = Character.objects.all()
+    # serializer = CharacterSerializer(characters, many=True)
+    return Response(characters)
